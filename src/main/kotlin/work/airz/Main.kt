6120790fc06a883a79a4af74e8f121e742639fc1
khrom <devs@outlook.jp>
2018-02-28 16:32:02 +0900

    add csvのrw機能を追加

--- src/main/kotlin/work/airz/Main.kt
+++ src/main/kotlin/work/airz/Main.kt
@@ -7,11 +7,16 @@ import java.io.*
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
 import javax.imageio.ImageIO
-import kotlin.math.pow
 
 
 fun main(args: Array<String>) {
-    videoProcessing(File("./"))
+//    videoProcessing(File("./"))
+//    var videoHash = loadHashMap(File("./hashdb.bin"))
+//    if (videoHash != null)
+//        save2csv(File("./csv"), videoHash)
+    loadcsv(File("./csv"))!!.forEach { key, value ->
+        println("hash:${String.format("%016X", key)} value:${value}")
+    }
 }
 
 /**
@@ -21,7 +26,7 @@ fun videoProcessing(rootDir: File) {
     if (!rootDir.isDirectory) return
     var videoHash = HashMap<Long, MutableList<String>>(4000000, 1.0F) //100話分くらいだったらこれくらいかと(要検証)
 
-    var fileList = recursiveSearch(rootDir).filter(::nameCheck)
+    var fileList = recursiveSearch(rootDir).filter { file -> nameCheck(file, "mp4") }
     println("${fileList.size} video files are found.")
     fileList.forEach {
         var miniVideoHash = video2Hash(it)
@@ -40,10 +45,10 @@ fun videoProcessing(rootDir: File) {
     videoHash.forEach { key, value ->
         println("hash:${String.format("%016X", key)} value:${value}")
     }
-    saveHashMap(File("./hashdb.bin"), videoHash)
-    loadHashMap(File("./hashdb.bin"))!!.forEach { key, value ->
-        println("hash:${String.format("%016X", key)} value:${value}")
-    }
+//    saveHashMap(File("./hashdb.bin"), videoHash)
+//    loadHashMap(File("./hashdb.bin"))!!.forEach { key, value ->
+//        println("hash:${String.format("%016X", key)} value:${value}")
+//    }
 
 }
 
@@ -81,9 +86,9 @@ fun recursiveSearch(rootDir: File): MutableList<File> {
  * ファイル名が正しいか確認
  * @param videoFile 確認する対象
  */
-fun nameCheck(videoFile: File): Boolean {
+fun nameCheck(videoFile: File, ext: String): Boolean {
     if (!videoFile.exists() || videoFile.isDirectory) return false //存在確認
-    if (!videoFile.extension.equals("mp4")) return false //拡張子確認
+    if (!videoFile.extension.equals(ext)) return false //拡張子確認
     if (videoFile.nameWithoutExtension.split("_").size != 2) return false //ファイル名は titleId_storyId (.mp4) という制限
     if (videoFile.nameWithoutExtension.split("_").any { it.equals("") }) return false //titleId/storyIdに空があってはならない
     return true
@@ -130,8 +135,42 @@ fun video2Hash(input: File): HashMap<Long, MutableList<String>> {
 /**
  * videoのハッシュを書き出します。
  */
-fun save2csv(videoHash: HashMap<Long, String>) {
+fun save2csv(rootDir: File, videoHash: HashMap<Long, MutableList<String>>) {
+    rootDir.mkdirs()
+    videoHash.forEach { key, value ->
+        //ファイル形式 value:　titleId_storyId_frame
+        value.forEach {
+            var splittedValue = it.split("_")
+            var outputFile = File(rootDir, "${splittedValue[0]}_${splittedValue[1]}.csv") //出力先の作成
+            if (!outputFile.parentFile.exists()) outputFile.parentFile.mkdirs() //出力先がない場合に作成する
+            outputFile.appendText("${key},${splittedValue[2]}\n")
+        }
+    }
+}
 
+fun loadcsv(rootDir: File): HashMap<Long, MutableList<String>> {
+    var csvFiles = recursiveSearch(rootDir).filter { file -> nameCheck(file, "csv") }
+    var videoHash = HashMap<Long, MutableList<String>>(4000000, 1.0F)
+    csvFiles.forEach { csvFile ->
+        val tId_sId = csvFile.nameWithoutExtension // titleId_storyId
+        csvFile.readLines().forEach {
+            val splittedText = it.split(",") //csvの分割
+            //0がハッシュ値,1がフレーム
+            val key = splittedText[0].toLong()
+            val value = "${tId_sId}_${splittedText[1]}"
+
+            val hashListExists = videoHash[key] //あるか確認
+            if (hashListExists != null && hashListExists.isNotEmpty()) {
+                hashListExists.add(value)
+                videoHash[key] = hashListExists
+            } else { //新たなやつだった場合
+                var hashList = mutableListOf<String>()
+                hashList.add(value)
+                videoHash[key] = hashList //新たに追加
+            }
+        }
+    }
+    return videoHash
 }
 
 /**
@@ -143,7 +182,6 @@ fun save2csv(videoHash: HashMap<Long, String>) {
  */
 fun save2jpg(input: File, rootDir: File, bufferedImage: BufferedImage, frameNumber: Int): Boolean {
     if (!(rootDir.exists() && rootDir.isDirectory)) rootDir.mkdirs() //ディレクトリ作成
-
     var outputFolder = File(rootDir, input.nameWithoutExtension)
     outputFolder.mkdirs()
     return try {
@@ -154,21 +192,3 @@ fun save2jpg(input: File, rootDir: File, bufferedImage: BufferedImage, frameNumb
         false
     }
 }
-//fun video2Hash(input: File) {
-//    var fileChannel = NIOUtils.readableChannel(input)
-//    var frameGrabber = FrameGrab.createFrameGrab(fileChannel)
-//    var count = 0
-//    var start = System.currentTimeMillis()
-//
-////    if (!File("./jpgout").exists()) File("./jpgout").mkdirs()
-//
-//    for (i in 0 until frameGrabber.videoTrack.meta.totalFrames) {
-//        var picture = frameGrabber.nativeFrame
-////        ImageIO.write(AWTUtil.toBufferedImage(picture), "jpg", File("./jpgout", "${input.name.replaceAfterLast(input.extension, "")}_${i}.jpg"))
-//
-////        println(ImageSearch().getVector(AWTUtil.toBufferedImage(picture)).toString(16))
-//        if (i > 50) break //5771[ms]
-//    }
-//    var end = System.currentTimeMillis()
-//    println("${end - start}[ms]")
-//}
